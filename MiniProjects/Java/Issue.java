b402e900649dd83d8a89fc219bf1bdf2b1e17fa4
AMBERPETA JAYA SHREE <ajayashree556@gmail.com>
2026-04-24 12:03:07 +0530

    Add Issue class for book borrowing management

---
+++ MiniProjects/Java/Issue.java
@@ -0,0 +1,11 @@
+class Issue {
+    int bookId;
+    String userId;
+    int days;
+
+    Issue(int bookId, String userId, int days) {
+        this.bookId = bookId;
+        this.userId = userId;
+        this.days = days;
+    }
+}
